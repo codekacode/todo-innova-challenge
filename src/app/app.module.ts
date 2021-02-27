047713c59e0a7b76212ed4154b9cb9fdc7f7c17d
Codekacode <karem.quiroz.m@gmail.com>
2021-02-27 14:54:37 -0500

    create board detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { BoardDetailComponent } from './pages/board-detail/board-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    BoardDetailComponent
   ],
   imports: [
     BrowserModule,
