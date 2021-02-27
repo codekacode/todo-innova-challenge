cbfefae236cf8bc3d6bfae1a441ae501ac75c6e2
Codekacode <karem.quiroz.m@gmail.com>
2021-02-27 16:04:47 -0500

    Add rout to e home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'My Boards';
+
+  ngOnInit(){
+    this.title = "Hola"
+  }
 }
+
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  Title = "My Boards";
+  Title = "Trello";
   trello = []
   constructor() { }
 
